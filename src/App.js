d84a1a677682d3a522ff4ac39c62d415c7d1456a
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-02-20 09:32:01 +0200

    1. weather-test - React projekti loomine ja käivitamine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import setupMirageServer from "./mirage";
 import './App.css';
 
+setupMirageServer();
+
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(`/api/weather?lat=44.34&lon=10.99`)
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json); 
+        setWeatherData(json);
+      })
+      .catch((error) => {
+        console.error('Error fetching weather data:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Learn React
+      {weatherData && (
+        <div>
+          <p>Temperature: {weatherData.main.temp}</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 }
